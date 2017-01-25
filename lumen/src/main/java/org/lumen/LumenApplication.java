3e64f888db1a9722a52af86a61ad3c8a6e795dce
ramir <ramir@ramir-L64>
2017-01-25 09:30:16 -0300

    initial commit

---
+++ lumen/src/main/java/org/lumen/LumenApplication.java
@@ -0,0 +1,12 @@
+package org.lumen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumenApplication.class, args);
+	}
+}
---
+++ lumen/src/test/java/org/lumen/LumenApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lumen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LumenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
